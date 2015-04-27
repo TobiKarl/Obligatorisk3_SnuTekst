e78e02a8d523d00eaaf20d2243528e7fc6bb1b22
TobiasK <tobi.karl@hotmail.com>
2015-04-27 17:25:34 +0200

    Initial commit

---
+++ SnuTekst/src/del3_1/SnuTekst.java
@@ -0,0 +1,31 @@
+package del3_1;
+
+class SnuTekst {
+   /*Oppgave 1
+	Lag et nytt prosjekt og legg dette inn i din GIT repository.
+	Oppgaven går ut på å skrive ut tegnene i en streng i motsatt rekkefølge (Basert på Exercise 18.9 fra lab onsdag, men med noen modifikasjoner)
+	Klassen skal hete SnuTekst. Skriv en rekursiv metode som viser innholdet i en streng baklengs. Metoden skal ha følgende header:
+	public static void baklengs(String tekst)
+	Skriv et testprogram (main-metode) som ber brukeren om en streng. Ledeteksten skal være på norsk. Strengen skal så skrives ut baklengs med en ledetekst på norsk.
+	Når dette fungerer utgjør dette første utgave av klassen og skal ha versjon 0.1. Gjør en Commit på dette i GIT 
+	---
+	Gjør en branch i GIT og legg til funksjonalitet for å telle antall tegn ved utskrift av hvert tegn. Resultatet skal skrives ut (med passende ledetekst) etter at strengen er skrevet ut baklengs.
+	Flere av oppgavene i kapitlet har tips til hvordan man løser tilsvarende problemstillinger.
+	Når dette fungerer utgjør det andre utgave av klassen. Gjør en Commit og foreta en merge mot master til versjon 0.2.
+	----
+	Gjør en ny branch i GIT og omarbeid programmet slik at det bruker en hjelpemetode for den rekursive metoden.
+	Hensikten er at den rekursive metoden ikke skal lage en ny streng for hvert kall. For å oppnå dette skal den rekursive metoden nå ha headeren:
+	public static void baklengs(String tekst, int siste) 
+	Hjelpemetoden skal ha samme header som den rekursive hadde tidligere.
+	Når programmet fungerer som forventet og du er fornøyd, gjør Commit og foreta en merge mot master til versjon 0.3. Publiser løsningen på GitHub.
+	*/
+
+	
+	public static void baklengs(String tekst){
+		
+	}
+	
+	public static void main(String[] args) {
+	
+	}
+}
